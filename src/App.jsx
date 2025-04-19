83206eb02a62bac5d9519f900f351e0b6670ab60
MayaRozenberg <maya.rozen53@gmail.com>
2025-04-19 16:40:40 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import TranslatorStart from "./components/TranslatorStart";
 import TranslatorApp from "./components/TranslatorApp";
 
 const App = () => {
+  const [showTranslatorApp, setShowTranslatorApp] = useState(false);
+
   return (
-    <div className="w-full h-screen bg-gradient-to-l from-[#b6f492] to-[#338b93] flex justify-center items-center">
-      <div className="w-[90%] max-w-lg  bg-[#2d2d2d] rounded-xl shadow-2xl shadow-gray-800 flex flex-col">
-        {/*<TranslatorStart />*/}
-        <TranslatorApp />
+    <div className="w-full h-screen bg-gradient-to-l from-[#3F5185] to-[#A1C6EA] flex justify-center items-center">
+      <div className="w-[90%] max-w-lg max-[392px]:h-[90%] sm:h-auto bg-[#2d2d2d] rounded-xl shadow-2xl shadow-gray-800 flex flex-col">
+        {showTranslatorApp ? (
+          <TranslatorApp onClose={() => setShowTranslatorApp(false)} />
+        ) : (
+          <TranslatorStart onStart={() => setShowTranslatorApp(true)} />
+        )}
       </div>
     </div>
   );
--- src/components/TranslatorStart.jsx
+++ src/components/TranslatorStart.jsx
@@ -1,26 +1,40 @@
-const TranslatorStart = () => {
+const TranslatorStart = ({ onStart }) => {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center p-12 ">
-      <div className="w-full h-64 bg-gradient-to-l from-[#b6f492] to-[#338b93] rounded-t-full rounded-bl-full flex flex-col justify-center text-gray-700 pr-6">
-        <span className="font-shojumaru text-6xl text-center">Hello</span>
-        <span className="font-bellefair text-4xl text-center">שלום</span>
-        <span className="font-notoSanaJP text-3xl text-right">こんにちは</span>
-        <span className="text-2xl text-right">Hola</span>
+    <div className="w-full h-full flex flex-col justify-center items-center p-6 sm:p-12 ">
+      <div className="w-full h-64 bg-gradient-to-l from-[#3F5185] to-[#A1C6EA] rounded-t-full rounded-bl-full flex flex-col justify-center text-gray-700 pr-6 translate-x-2 text-center">
+        <span className="font-shojumaru text-5xl sm:text-6xl">Hello</span>
+        <span className="font-bellefair sm:text-4xl">שלום</span>
+        <span className="font-notoSanaJP text-3xl sm:text-4xl text-right">
+          こんにちは
+        </span>
+        <span className="text-2xl sm:text-3xl text-right">Hola</span>
       </div>
       <div className="w-full text-right space-y-5 mt-20 mb-36">
-        <h1 className="font-righteous text-4xl text-white uppercase">
-          Translator App
-        </h1>
-        <button className="w-32 h-10 bg-gradient-to-r from-[#b6f492] to-[#338b93] rounded-full font-righteous font-bole text-lg uppercase text-gray-700 tracking-widest active:translate-y-[1px]">
-          Start
+        <h1 className="font-righteous text-4xl text-white uppercase">Wordly</h1>
+
+        <button
+          className="
+            w-32 h-10 
+            bg-gradient-to-r from-[#3F5185] to-[#A1C6EA] 
+            rounded-full 
+            font-righteous font-bold text-lg uppercase 
+            text-white 
+            tracking-widest 
+            shadow-md hover:shadow-lg
+            hover:from-[#4B5D8A] hover:to-[#B4D0F0]
+            transition-all duration-200 ease-in-out 
+            active:translate-y-[1px] active:brightness-90
+          "
+          onClick={onStart}
+        >
+          <span className="mr-1">🚀</span>Start
         </button>
       </div>
+      <p className="absolute bottom-4 left-4 text-sm text-gray-800">
+        by Maya Shahaf Rozenberg 💻
+      </p>{" "}
     </div>
   );
 };
 
 export default TranslatorStart;
-
-{
-  /*bellefair --------- עברית*/
-}
